5c9c1f7ccbf4b6d6b4626944d5fb264cd1167e95
qf <qf@qfdemacbook-pro.local>
2017-03-01 17:17:12 +0800

    Initial Commit

---
+++ SXRSDKDemoKKasong/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SXRSDKDemoKKasong
+//
+//  Created by qf on 2017/3/1.
+//  Copyright © 2017年 qf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SXRSDKDemoKKasong/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SXRSDKDemoKKasong
+//
+//  Created by qf on 2017/3/1.
+//  Copyright © 2017年 qf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
